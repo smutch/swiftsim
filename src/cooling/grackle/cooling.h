01cd315380766a570655382b89f05c6ca67314fb
loikki <loic.hausammann@protonmail.ch>
2019-07-31 11:06:48 +0200

    GEAR: feedback almost working

--- src/cooling/grackle/cooling.h
+++ src/cooling/grackle/cooling.h
@@ -25,6 +25,7 @@
  */
 
 /* Some standard headers. */
+#include <fenv.h>
 #include <float.h>
 #include <math.h>
 
@@ -932,6 +933,8 @@ __attribute__((always_inline)) INLINE static void cooling_init_backend(
     const struct phys_const* phys_const,
     struct cooling_function_data* cooling) {
 
+  fedisableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
+
   if (GRACKLE_NPART != 1)
     error("Grackle with multiple particles not implemented");
 
@@ -943,6 +946,8 @@ __attribute__((always_inline)) INLINE static void cooling_init_backend(
 
   /* Set up grackle */
   cooling_init_grackle(cooling);
+
+  feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
 }
 
 /**
