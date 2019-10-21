a202477523ca470a20dfd5762c17ad030023f64c
Simon Mutch <smutch@unimelb.edu.au>
2019-10-21 17:00:36 +1100

    Format new test code to match style

--- tests/testDarkMatterWriteGrids.c
+++ tests/testDarkMatterWriteGrids.c
@@ -6,37 +6,35 @@
 static char fname[] = {"testDarkMatterWriteGrids.h5"};
 static int grid_dim = 16;
 
-static int isclose(double x, double y) {
-  return (fabs(x - y) < 1e-8);
-}
+static int isclose(double x, double y) { return (fabs(x - y) < 1e-8); }
 
 static int row_major_id_periodic(int i, int j, int k, int N) {
-    return (((i + N) % N) * N * N + ((j + N) % N) * N + ((k + N) % N));
+  return (((i + N) % N) * N * N + ((j + N) % N) * N + ((k + N) % N));
 }
 
 static hid_t create_output_file(void) {
-    hid_t plist_id = H5Pcreate(H5P_FILE_ACCESS);
-    H5Pset_fapl_mpio(plist_id, MPI_COMM_WORLD, MPI_INFO_NULL);
-    hid_t h_file = H5Fcreate(fname, H5F_ACC_TRUNC, H5P_DEFAULT, plist_id);
-    H5Pclose(plist_id);
-    hid_t h_grp = H5Gcreate(h_file, "PartType1", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-    H5Gclose(h_grp);
+  hid_t plist_id = H5Pcreate(H5P_FILE_ACCESS);
+  H5Pset_fapl_mpio(plist_id, MPI_COMM_WORLD, MPI_INFO_NULL);
+  hid_t h_file = H5Fcreate(fname, H5F_ACC_TRUNC, H5P_DEFAULT, plist_id);
+  H5Pclose(plist_id);
+  hid_t h_grp =
+      H5Gcreate(h_file, "PartType1", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  H5Gclose(h_grp);
 
-    return h_file;
+  return h_file;
 }
 
 static void read_results(const char ds_name[], float *data) {
 
-    hid_t h_file = H5Fopen(fname, H5F_ACC_RDONLY, H5P_DEFAULT);
+  hid_t h_file = H5Fopen(fname, H5F_ACC_RDONLY, H5P_DEFAULT);
 
-    char ds[256];
-    snprintf(ds, 256, "/PartType1/Grids/%s", ds_name);
+  char ds[256];
+  snprintf(ds, 256, "/PartType1/Grids/%s", ds_name);
 
-    H5LTread_dataset_float(h_file, ds, data);
+  H5LTread_dataset_float(h_file, ds, data);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   const int Npart = 10;
   const double dim[3] = {100., 100., 100.};
   const int n_threads = 1;
@@ -46,13 +44,13 @@ int main(int argc, char *argv[])
 
   int i_rank, n_ranks;
   MPI_Comm_rank(MPI_COMM_WORLD, &i_rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &n_ranks); 
+  MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
 
   struct unit_system internal_units, snapshot_units;
   units_init(&internal_units, 1.988480e+43, 3.085678e+24, 3.085678e+19,
-      1.000000e+00, 1.000000e+00);
+             1.000000e+00, 1.000000e+00);
   units_init(&snapshot_units, 1.988480e+43, 3.085678e+24, 3.085678e+19,
-      1.000000e+00, 1.000000e+00);
+             1.000000e+00, 1.000000e+00);
 
   struct engine engine;
   struct space space;
@@ -60,175 +58,182 @@ int main(int argc, char *argv[])
   engine.snapshot_grid_dim = grid_dim;
   threadpool_init(&engine.threadpool, n_threads);
 
-  for(int ii=0; ii<3; ++ii) {
-      space.dim[ii] = dim[ii];
+  for (int ii = 0; ii < 3; ++ii) {
+    space.dim[ii] = dim[ii];
   }
-  struct gpart* gparts = calloc(Npart, sizeof(struct gpart));
-  space.gparts = (struct gpart*)gparts;
+  struct gpart *gparts = calloc(Npart, sizeof(struct gpart));
+  space.gparts = (struct gpart *)gparts;
 
   float *res;
-  if(i_rank == 0) {
+  if (i_rank == 0) {
     res = calloc(n_cells, sizeof(float));
   }
 
   /* TEST CASE 1 */
   {
-      snprintf(engine.snapshot_grid_method, 4, "NGP");
+    snprintf(engine.snapshot_grid_method, 4, "NGP");
 
-      for(int ii=0; ii<Npart; ++ii) {
-          for(int jj=0; jj<3; ++jj) {
-              gparts[ii].x[jj] = 0.;
-              gparts[ii].v_full[jj] = 2. * jj;
-          }
-          gparts[ii].mass = 10.;
+    for (int ii = 0; ii < Npart; ++ii) {
+      for (int jj = 0; jj < 3; ++jj) {
+        gparts[ii].x[jj] = 0.;
+        gparts[ii].v_full[jj] = 2. * jj;
+      }
+      gparts[ii].mass = 10.;
+    }
+
+    hid_t h_file = create_output_file();
+    darkmatter_write_grids(&engine, (size_t)Npart, h_file, &internal_units,
+                           &snapshot_units);
+    H5Fclose(h_file);
+
+    if (i_rank == 0) {
+      read_results("Density", res);
+
+      float total = 0;
+      for (int ii = 0; ii < n_cells; ++ii) {
+        total += res[ii];
+      }
+      assert(isclose(total, 0.8192f));
+
+      assert(isclose(res[0], 0.8192f));
+      for (int ii = 1; ii < n_cells; ++ii) {
+        assert(isclose(res[ii], 0.0f));
       }
 
-      hid_t h_file = create_output_file();
-      darkmatter_write_grids(&engine, (size_t)Npart, h_file, &internal_units, &snapshot_units);
-      H5Fclose(h_file);
+      read_results("Vx", res);
+      for (int ii = 0; ii < n_cells; ++ii) {
+        assert(isclose(res[ii], 0.0f));
+      }
 
-      if (i_rank == 0) {
-          read_results("Density", res);
+      read_results("Vy", res);
+      assert(isclose(res[0], 2.0f));
+      for (int ii = 1; ii < n_cells; ++ii) {
+        assert(isclose(res[ii], 0.0f));
+      }
 
-          float total = 0;
-          for(int ii=0; ii<n_cells; ++ii) {
-              total += res[ii];
-          }
-          assert(isclose(total, 0.8192f));
-
-          assert(isclose(res[0], 0.8192f));
-          for(int ii = 1; ii < n_cells; ++ii) {
-              assert(isclose(res[ii], 0.0f));
-          }
-
-          read_results("Vx", res);
-          for(int ii = 0; ii < n_cells; ++ii) {
-              assert(isclose(res[ii], 0.0f));
-          }
-
-          read_results("Vy", res);
-          assert(isclose(res[0], 2.0f));
-          for(int ii = 1; ii < n_cells; ++ii) {
-              assert(isclose(res[ii], 0.0f));
-          }
-
-          read_results("Vz", res);
-          assert(isclose(res[0], 4.0f));
-          for(int ii = 1; ii < n_cells; ++ii) {
-              assert(isclose(res[ii], 0.0f));
-          }
-      }   
+      read_results("Vz", res);
+      assert(isclose(res[0], 4.0f));
+      for (int ii = 1; ii < n_cells; ++ii) {
+        assert(isclose(res[ii], 0.0f));
+      }
+    }
   }
 
   /* TEST CASE 2 */
   {
-      snprintf(engine.snapshot_grid_method, 4, "CIC");
-      if (i_rank == 0) {
-          memset(res, 0, sizeof(float)*n_cells);
+    snprintf(engine.snapshot_grid_method, 4, "CIC");
+    if (i_rank == 0) {
+      memset(res, 0, sizeof(float) * n_cells);
+    }
+
+    hid_t h_file = create_output_file();
+    darkmatter_write_grids(&engine, (size_t)Npart, h_file, &internal_units,
+                           &snapshot_units);
+    H5Fclose(h_file);
+
+    if (i_rank == 0) {
+      read_results("Density", res);
+
+      float total = 0;
+      for (int ii = 0; ii < n_cells; ++ii) {
+        total += res[ii];
+      }
+      assert(isclose(total, 0.8192f));
+
+      assert(isclose(res[0], 0.8192f));
+      for (int ii = 1; ii < n_cells; ++ii) {
+        assert(isclose(res[ii], 0.0f));
       }
 
-      hid_t h_file = create_output_file();
-      darkmatter_write_grids(&engine, (size_t)Npart, h_file, &internal_units, &snapshot_units);
-      H5Fclose(h_file);
+      read_results("Vx", res);
+      for (int ii = 0; ii < n_cells; ++ii) {
+        assert(isclose(res[ii], 0.0f));
+      }
 
-      if (i_rank == 0) {
-          read_results("Density", res);
+      read_results("Vy", res);
+      assert(isclose(res[0], 2.0f));
+      for (int ii = 1; ii < n_cells; ++ii) {
+        assert(isclose(res[ii], 0.0f));
+      }
 
-          float total = 0;
-          for(int ii=0; ii<n_cells; ++ii) {
-              total += res[ii];
-          }
-          assert(isclose(total, 0.8192f));
-
-          assert(isclose(res[0], 0.8192f));
-          for(int ii = 1; ii < n_cells; ++ii) {
-              assert(isclose(res[ii], 0.0f));
-          }
-
-          read_results("Vx", res);
-          for(int ii = 0; ii < n_cells; ++ii) {
-              assert(isclose(res[ii], 0.0f));
-          }
-
-          read_results("Vy", res);
-          assert(isclose(res[0], 2.0f));
-          for(int ii = 1; ii < n_cells; ++ii) {
-              assert(isclose(res[ii], 0.0f));
-          }
-
-          read_results("Vz", res);
-          assert(isclose(res[0], 4.0f));
-          for(int ii = 1; ii < n_cells; ++ii) {
-              assert(isclose(res[ii], 0.0f));
-          }
-      }   
+      read_results("Vz", res);
+      assert(isclose(res[0], 4.0f));
+      for (int ii = 1; ii < n_cells; ++ii) {
+        assert(isclose(res[ii], 0.0f));
+      }
+    }
   }
 
   /* TEST CASE 3 */
   {
-      snprintf(engine.snapshot_grid_method, 4, "NGP");
-      if (i_rank == 0) {
-          memset(res, 0, sizeof(float)*n_cells);
+    snprintf(engine.snapshot_grid_method, 4, "NGP");
+    if (i_rank == 0) {
+      memset(res, 0, sizeof(float) * n_cells);
+    }
+
+    for (int ii = 0; ii < Npart; ++ii) {
+      for (int jj = 0; jj < 3; ++jj) {
+        gparts[ii].x[jj] = (float)jj * 2;
+        gparts[ii].v_full[jj] = 2. * jj;
       }
+    }
 
-      for(int ii=0; ii<Npart; ++ii) {
-          for(int jj=0; jj<3; ++jj) {
-              gparts[ii].x[jj] = (float)jj*2;
-              gparts[ii].v_full[jj] = 2. * jj;
-          }
+    hid_t h_file = create_output_file();
+    darkmatter_write_grids(&engine, (size_t)Npart, h_file, &internal_units,
+                           &snapshot_units);
+    H5Fclose(h_file);
+
+    if (i_rank == 0) {
+      read_results("Density", res);
+
+      float total = 0;
+      for (int ii = 0; ii < n_cells; ++ii) {
+        total += res[ii];
       }
+      assert(isclose(total, 0.8192f));
+      assert(isclose(res[row_major_id_periodic(0, 0, 1, grid_dim)], 0.8192f));
 
+      read_results("Vy", res);
+      assert(isclose(res[row_major_id_periodic(0, 0, 1, grid_dim)], 2.0f));
 
-      hid_t h_file = create_output_file();
-      darkmatter_write_grids(&engine, (size_t)Npart, h_file, &internal_units, &snapshot_units);
-      H5Fclose(h_file);
-
-      if (i_rank == 0) {
-          read_results("Density", res);
-
-          float total = 0;
-          for(int ii=0; ii<n_cells; ++ii) {
-              total += res[ii];
-          }
-          assert(isclose(total, 0.8192f));
-          assert(isclose(res[row_major_id_periodic(0, 0, 1, grid_dim)], 0.8192f));
-
-          read_results("Vy", res);
-          assert(isclose(res[row_major_id_periodic(0, 0, 1, grid_dim)], 2.0f));
-
-          read_results("Vz", res);
-          assert(isclose(res[row_major_id_periodic(0, 0, 1, grid_dim)], 4.0f));
-      }   
+      read_results("Vz", res);
+      assert(isclose(res[row_major_id_periodic(0, 0, 1, grid_dim)], 4.0f));
+    }
   }
-          
+
   /* TEST CASE 4 */
   {
-      snprintf(engine.snapshot_grid_method, 4, "CIC");
-      if (i_rank == 0) {
-          memset(res, 0, sizeof(float)*n_cells);
+    snprintf(engine.snapshot_grid_method, 4, "CIC");
+    if (i_rank == 0) {
+      memset(res, 0, sizeof(float) * n_cells);
+    }
+
+    hid_t h_file = create_output_file();
+    darkmatter_write_grids(&engine, (size_t)Npart, h_file, &internal_units,
+                           &snapshot_units);
+    H5Fclose(h_file);
+
+    if (i_rank == 0) {
+      read_results("Density", res);
+
+      float total = 0;
+      for (int ii = 0; ii < n_cells; ++ii) {
+        total += res[ii];
       }
-
-      hid_t h_file = create_output_file();
-      darkmatter_write_grids(&engine, (size_t)Npart, h_file, &internal_units, &snapshot_units);
-      H5Fclose(h_file);
-
-      if (i_rank == 0) {
-          read_results("Density", res);
-
-          float total = 0;
-          for(int ii=0; ii<n_cells; ++ii) {
-              total += res[ii];
-          }
-          assert(isclose(total, 0.8192f));
-          assert(isclose(res[row_major_id_periodic(0, 0, 0, grid_dim)], 0.20054016f));
-          assert(isclose(res[row_major_id_periodic(0, 0, 1, grid_dim)], 0.35651584f));
-          assert(isclose(res[row_major_id_periodic(0, 1, 0, grid_dim)], 0.09437184f));
-          assert(isclose(res[row_major_id_periodic(0, 1, 1, grid_dim)], 0.16777216f));
-      }   
+      assert(isclose(total, 0.8192f));
+      assert(
+          isclose(res[row_major_id_periodic(0, 0, 0, grid_dim)], 0.20054016f));
+      assert(
+          isclose(res[row_major_id_periodic(0, 0, 1, grid_dim)], 0.35651584f));
+      assert(
+          isclose(res[row_major_id_periodic(0, 1, 0, grid_dim)], 0.09437184f));
+      assert(
+          isclose(res[row_major_id_periodic(0, 1, 1, grid_dim)], 0.16777216f));
+    }
   }
-          
-  if(i_rank == 0) {
-      free(res);
+
+  if (i_rank == 0) {
+    free(res);
   }
 
   free(gparts);
